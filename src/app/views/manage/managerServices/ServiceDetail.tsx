dc1b690be91ba059ff22323fd2d8b2a64c75988b
AnhVu910 <conanshinichi2000@gmail.com>
2022-11-04 09:19:28 +0700

    fix: commit

--- src/app/views/manage/managerServices/ServiceDetail.tsx
+++ src/app/views/manage/managerServices/ServiceDetail.tsx
@@ -102,7 +102,7 @@ export default function ServiceDetail(props: Props) {
     camp: Yup.object().nullable().required(messages.MSG1),
     rentalType: Yup.string().required(messages.MSG1),
     capacity: Yup.string()
-      .max(4, 'Chỉ được nhập tối đa 4 ký tự')
+      .max(5, 'Chỉ được nhập tối đa 4 chữ số')
       .required(messages.MSG1),
     name: Yup.string()
       .max(255, 'Chỉ được nhập tối đa 255 ký tự')

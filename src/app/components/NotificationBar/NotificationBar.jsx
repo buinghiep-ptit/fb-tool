8783adc6a455e970f95da63e7dfd6235d28bf9ba
NGHIEP BUI <nghiepbv2@fpt.com.vn>
2022-11-24 08:29:27 +0700

    fix: hidden notifications

--- src/app/components/NotificationBar/NotificationBar.jsx
+++ src/app/components/NotificationBar/NotificationBar.jsx
@@ -114,7 +114,7 @@ const NotificationBar = ({ container }) => {
           <Box sx={{ width: sideNavWidth }}>
             <Notification>
               <Icon color="primary">notifications</Icon>
-              <h5>Notifications</h5>
+              <h5>Thông báo</h5>
             </Notification>
 
             {notifications?.map(notification => (
@@ -156,9 +156,7 @@ const NotificationBar = ({ container }) => {
             ))}
             {!!notifications?.length && (
               <Box sx={{ color: secondary }}>
-                <Button onClick={clearNotifications}>
-                  Clear Notifications
-                </Button>
+                <Button onClick={clearNotifications}>Xoá thông báo</Button>
               </Box>
             )}
           </Box>

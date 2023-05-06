f263e8b1dfae5a2c4cdf2d053b4c880dcb6468fe
AnhVu910 <conanshinichi2000@gmail.com>
2023-05-06 17:47:00 +0700

    fix: delete banner

--- src/app/apis/banner/banner.service.ts
+++ src/app/apis/banner/banner.service.ts
@@ -8,3 +8,7 @@ export const sortBanner = async (params: any): Promise<any> => {
   const { data } = await http.put<any>('/api/banners/priority', params)
   return data
 }
+export const deleteBannerI = async (id: any): Promise<any> => {
+  const { data } = await http.delete<any>(`/api/banners/${id}`)
+  return data
+}
--- src/app/views/manager/banner/BannerManager.tsx
+++ src/app/views/manager/banner/BannerManager.tsx
@@ -9,9 +9,14 @@ import {
   Tooltip,
 } from '@mui/material'
 import { Box } from '@mui/system'
-import { getListBanner, sortBanner } from 'app/apis/banner/banner.service'
+import {
+  deleteBannerI,
+  getListBanner,
+  sortBanner,
+} from 'app/apis/banner/banner.service'
 import { Breadcrumb, Container, SimpleCard } from 'app/components'
 import { MuiButton } from 'app/components/common/MuiButton'
+import { toastSuccess } from 'app/helpers/toastNofication'
 import { useNavigateParams } from 'app/hooks/useNavigateParams'
 import React, { useState } from 'react'
 import RLDD from 'react-list-drag-and-drop/lib/RLDD'
@@ -50,7 +55,19 @@ export default function BannerManager(props: Props) {
     setIsloading(true)
     const res = await sortBanner({ priorityMap })
     if (res) {
+      toastSuccess({
+        message: 'Cập nhật thành công',
+      })
       setIsloading(false)
+      fetBanner()
+    }
+  }
+  const deleteBanner = async (id: any) => {
+    const res = await deleteBannerI(id)
+    if (res) {
+      toastSuccess({
+        message: 'Xoá thành công',
+      })
       fetBanner()
     }
   }
@@ -102,7 +119,11 @@ export default function BannerManager(props: Props) {
             </IconButton>
           </Tooltip>
           <Tooltip title="Xóa" placement="top">
-            <IconButton color="primary" sx={{ width: '33%' }}>
+            <IconButton
+              color="primary"
+              sx={{ width: '33%' }}
+              onClick={() => deleteBanner(banner.id)}
+            >
               <Delete />
             </IconButton>
           </Tooltip>

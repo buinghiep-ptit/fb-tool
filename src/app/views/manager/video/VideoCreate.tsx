dbab67e595d9322bfeb18c9cda19038dd96a54ce
giangcm <giangcm@fpt.com.vn>
2023-05-23 15:45:18 +0700

    update ui

--- src/app/views/manager/video/VideoCreate.tsx
+++ src/app/views/manager/video/VideoCreate.tsx
@@ -345,7 +345,7 @@ export default function VideoCreate(props: Props) {
                         <BackupIcon fontSize="large" />
                         <div>PNG/JPEG hoặc JPG</div>
                         <div>Dung lượng không quá 50mb</div>
-                        <div>(Tỷ lệ ảnh phù hợp 3:2)</div>
+                        <div>(Tỷ lệ ảnh phù hợp 16:9)</div>
                       </div>
                     )}
                     {previewImage?.length !== 0 && (
